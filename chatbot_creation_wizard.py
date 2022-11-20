eeee9660dfddfb0262f0990051ba2912801315d1
Elias <stoyianel@gmail.com>
2022-11-20 22:32:03 +0200

    test

--- chatbot_creation_wizard.py
+++ chatbot_creation_wizard.py
@@ -13,7 +13,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from chromedriver_py import binary_path # this will get you the path variable
 import os 
 
-os.environ["PATH"] += os.pathsep + "chromedriver.exe"
+os.environ["PATH"] += os.pathsep + binary_path
 
 api_key = st.secrets["COHERE_API_KEY"]
 co = cohere.Client(api_key)
@@ -96,13 +96,13 @@ def get_html_raw(url: str) -> str:
 
 # Get the rendered HTML from a URL as it is seen from a browswer
 def get_html_chrome(url: str) -> str:
-  service_object = Service("chromedriver.exe")
+  service_object = Service(binary_path)
   # get the html using selenium headless browser
   chrome_options = webdriver.ChromeOptions()
   chrome_options.add_argument('--headless')
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--disable-dev-shm-usage')
-  driver = webdriver.Chrome("driver\chromedriver.exe")
+  driver = webdriver.Chrome(service=service_object, options=chrome_options)
   driver.get(url)
   html = driver.page_source
   return html
