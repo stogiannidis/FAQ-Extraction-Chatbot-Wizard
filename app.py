921795d78322513b48e904668e17747c915766c6
Elias <stoyianel@gmail.com>
2022-11-21 00:08:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,30 +6,29 @@ header = st.container()
 faq_extraction = st.container()
 
 with header:
-    st.title("FAQ Extraction bot creation wizard")
+    st.title("FAQ Generation wizard")
 
 # #create an input box for the user to enter a link 
 with faq_extraction:
-    st.header("FAQ Extraction")
     bot = dict
-    link = st.text_input("Enter a link to extract FAQ's from")
-    if st.button("Extract FAQs"):
-        st.write("FAQs extracted from the link are:")
+    link = st.text_input("Enter a URL")
+    if st.button("Generate FAQs"):
+        st.header("Generated content")
         bot = generate_faq_bot(link)
     
-        st.header(f"Company name: {bot['name']}")
+        st.title(f"Company name: {bot['name']}")
         st.write(f"The sector the company operates is: {bot['sector']}")
 
-        st.write(f"Question 1: {bot['questions'][0]}")
-        st.write(f"Answer 1: {bot['answers'][0]}")
-        st.write(f"Hyped answer 1: {bot['hyped'][0]}")
+        st.write(f"FAQ 1: {bot['questions'][0]}")
+        st.write(f"Answer: {bot['answers'][0]}")
+        st.write(f"Answer (hyped): {bot['hyped'][0]}")
         
-        st.write(f"Question 2: {bot['questions'][1]}")
-        st.write(f"Answer 2: {bot['answers'][1]}")
-        st.write(f"Hyped answer 2: {bot['hyped'][1]}")
+        st.write(f"FAQ 2: {bot['questions'][1]}")
+        st.write(f"Answer: {bot['answers'][1]}")
+        st.write(f"Answer (hyped): {bot['hyped'][1]}")
 
-        st.write(f"Question 3: {bot['questions'][2]}")
-        st.write(f"Answer 3: {bot['answers'][2]}")
-        st.write(f"Hyped answer 3: {bot['hyped'][2]}")
+        st.write(f"FAQ 3: {bot['questions'][2]}")
+        st.write(f"Answer: {bot['answers'][2]}")
+        st.write(f"Answer (hyped): {bot['hyped'][2]}")
 
 
